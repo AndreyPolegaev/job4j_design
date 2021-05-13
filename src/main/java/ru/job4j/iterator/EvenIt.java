23c3acdfd36e44b9fa6fcd198b09e0acf1a75c4f
AndreyPolegaev <79646307753@yandex.ru>
2021-05-13 11:38:09 +0300

    5.1.2. Создать итератор четные числа [#150 #88878]

--- src/main/java/ru/job4j/iterator/EvenIt.java
+++ src/main/java/ru/job4j/iterator/EvenIt.java
@@ -5,33 +5,39 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.function.Consumer;
 
-//public class EvenIt implements Iterator<Integer> {
+public class EvenIt implements Iterator<Integer> {
 
-//    private int[] numbers;
-//    int count = 0;
-//    int nextElement;
-//
-//    public EvenIt(final int[] numbers) {
-//        this.numbers = numbers;
-//    }
-//
-//    @Override
-//    public boolean hasNext() {
-//
-//        return false;
-//    }
-//
-//    @Override
-//    public Integer next() {
-//        if (!hasNext()) {
-//            throw new NoSuchElementException();
-//        }
-//
-//        return nextElement;
-//    }
-//
-//    @Override
-//    public void forEachRemaining(Consumer<? super Integer> action) {
-//      Arrays.stream(numbers).boxed().forEach(action);
-//    }
-//}
+    private int[] numbers;
+    int count = 0;
+    int nextElement;
+
+    public EvenIt(final int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    @Override
+    public boolean hasNext() {
+        for (int i = count; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                nextElement = numbers[i];
+                count = i;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        count++;
+        return nextElement;
+    }
+
+    @Override
+    public void forEachRemaining(Consumer<? super Integer> action) {
+      Arrays.stream(numbers).boxed().forEach(action);
+    }
+}
