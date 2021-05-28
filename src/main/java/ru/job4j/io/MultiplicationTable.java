5fb5dbe8bb12838a194be2dbe01c002192cb1b87
AndreyPolegaev <79646307753@yandex.ru>
2021-05-28 17:50:03 +0300

    0.1. FileOutputStream. [#252488 #161418]

--- src/main/java/ru/job4j/io/MultiplicationTable.java
+++ src/main/java/ru/job4j/io/MultiplicationTable.java
@@ -4,14 +4,13 @@ import java.io.FileOutputStream;
 
 public class MultiplicationTable {
     public static void main(String[] args) {
-
-        try {
-            FileOutputStream out = new FileOutputStream("D:\\ne.txt");
+        try (FileOutputStream out = new FileOutputStream("D:\\ne.txt")) {
+            String ln = System.lineSeparator();
             for (int i = 1; i < 10; i++) {
-                out.write("\n\n".getBytes());
                 for (int j = 1; j < 10; j++) {
                     out.write((i * j + "\t").getBytes());
                 }
+                out.write((ln + ln).getBytes());
             }
         } catch (Exception e) {
             e.printStackTrace();
