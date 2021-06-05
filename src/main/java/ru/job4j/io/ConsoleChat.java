c82970fa1088f75a78caf07c297e249e8aeedb34
AndreyPolegaev <79646307753@yandex.ru>
2021-06-05 23:33:30 +0300

    6. Кодировка. [#862]

---
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -0,0 +1,68 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ConsoleChat {
+
+    private final String path;
+    private final String pathAnswers;
+    private String[] answers = new String[0];
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+
+    public ConsoleChat(String path, String pathAnswers) {
+        this.path = path;
+        this.pathAnswers = pathAnswers;
+    }
+
+    public void run() {
+        String str = "";
+        try (Scanner in = new Scanner(System.in);
+             PrintWriter pw = new PrintWriter(new OutputStreamWriter(
+                     new FileOutputStream(path), StandardCharsets.UTF_8))) {
+            M:
+            while (!str.equals(OUT)) {
+                str = in.nextLine();
+                pw.println(str);
+                pw.flush();
+                if (str.equals(OUT)) {
+                    continue;
+                }
+                if (str.equals(STOP)) {
+                    while (!str.equals(CONTINUE)) {
+                        str = in.nextLine();
+                        pw.println(str);
+                        pw.flush();
+                        if (str.equals(OUT)) {
+                            break M;
+                        }
+                    }
+                }
+                pw.println(answer());
+                pw.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String answer() throws IOException {
+        if (answers.length == 0) {
+            answers = Files.lines(Path.of(pathAnswers), StandardCharsets.UTF_8).toArray(String[]::new);
+        }
+        String botAnswers = answers[new Random().nextInt(answers.length)];
+        System.out.println(botAnswers);
+        return botAnswers;
+    }
+
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat("dil.txt", "answers.txt");
+        cc.run();
+    }
+}
