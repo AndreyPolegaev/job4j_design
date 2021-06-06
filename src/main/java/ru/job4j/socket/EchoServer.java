1dc5ca0a9a5718df3d55410e8aa450b3a3d1d09b
AndreyPolegaev <79646307753@yandex.ru>
2021-06-07 00:24:13 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/socket/EchoServer.java
+++ src/main/java/ru/job4j/socket/EchoServer.java
@@ -3,24 +3,40 @@ package ru.job4j.socket;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
+                try (BufferedWriter out = new BufferedWriter(
+                        new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
+                    String answer = "";
                     String str = in.readLine();
                     while (!(str).isEmpty()) {
-                        if (str.startsWith("GET") && str.split(" ")[1].split("=")[1].equals("Bye")) {
-                            server.close();
-                        }
                         System.out.println(str);
+                        if (str.startsWith("GET") && str.contains("?msg=")) {
+                            answer = str.split(" ")[1].split("=")[1];
+                        }
                         str = in.readLine();
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\"".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n");
+                    out.flush();
+                    out.write("Hello, dear friend.\r\n\r\n");
+                    out.flush();
+                    if (answer.equals("Hello") || answer.equals("What")) {
+                        out.write(answer + "\r\n\r\n");
+                        out.flush();
+                    } else if (answer.equals("Exit")) {
+                        server.close();
+                    } else {
+                        out.write(answer + "\r\n\r\n");
+                        out.flush();
+                    }
+                    socket.close();
                 }
             }
         }
