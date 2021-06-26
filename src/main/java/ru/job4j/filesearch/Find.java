cf8ab2e057f8730dfddfa9160fe356190b24a5a9
AndreyPolegaev <79646307753@yandex.ru>
2021-06-26 22:54:37 +0300

    2. Поиск файлов по критерию [#783 #171492]

--- src/main/java/ru/job4j/filesearch/Find.java
+++ src/main/java/ru/job4j/filesearch/Find.java
@@ -1,35 +1,101 @@
 package ru.job4j.filesearch;
 
+/**
+ * Программа должна искать данные в заданном каталоге и подкаталогах.
+ * Имя файла может задаваться, целиком и по маске
+ * Программа должна собираться в jar и запускаться через java -jar find.jar -d=D:\\ -n=*.txt -t=mask -o=D:\\log.txt
+ * -d - директория, в которой начинать поиск.
+ * -n - имя файла, маска или реглярное выражение
+ * -t - тип поиска: mask искать по маске, name по полному совпадение имени, regex по регулярному выражению.
+ * -o - результат записать в файл.
+ */
+
 import ru.job4j.io.ArgsName;
 import java.awt.geom.IllegalPathStateException;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
-public class Find {
-    public static void main(String[] args) throws IOException {
+import static java.nio.file.FileVisitResult.CONTINUE;
 
-        /** валидация шаблона параметров и получение параметров */
-        ArgsName argsName = ArgsName.of(args);
+public class Find extends SimpleFileVisitor<Path> {
+
+    /**
+     * передаем предикат в зависимости от поиска по плному имени или маске или regex
+     */
+    private final Predicate<Path> conditions;
+
+    /**
+     * сохраняем ссылки на подходящие файлы для последющей записи в файл
+     */
+    private final List<Path> list = new ArrayList<>();
+
+    public Find(Predicate<Path> conditions) {
+        this.conditions = conditions;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (conditions.test(file)) {
+            list.add(file.toAbsolutePath());
+        }
+        return super.visitFile(file, attrs);
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return CONTINUE;
+    }
+
+    private static Predicate<Path> predicate(String type, String typeName) {
         Predicate<Path> conditions;
-
         /** типа файла: маска, полное имя или регулярное выражение */
-        if (argsName.get("t").equals("mask")) {
-            conditions = p -> p.toFile().getName().endsWith(argsName.get("n").split("\\.")[1]);
-        } else if (argsName.get("t").equals("name")) {
-            conditions = p -> p.toFile().getName().equals(argsName.get("n"));
-        } else if (argsName.get("t").equals("regex")) {
-            Pattern regexp = Pattern.compile(argsName.get("n")); // получаем регулярное выражение
+        if (type.equals("mask")) {
+            String rsl = typeName.replace("*", ".*").replace("?", ".");
+            rsl = rsl
+                    .chars()
+                    .mapToObj(s -> (char) s)
+                    .map(String::valueOf)
+                    .collect(Collectors.joining("", "^", "$"));
+            Pattern regexp = Pattern.compile(rsl); // получаем регулярное выражение из маски
+            conditions = p -> regexp.matcher(p.toFile().getName()).find();
+        } else if (type.equals("name")) {
+            conditions = p -> p.toFile().getName().equals(typeName);
+        } else if (type.equals("regex")) {
+            Pattern regexp = Pattern.compile(typeName); // получаем регулярное выражение
             conditions = p -> regexp.matcher(p.toFile().getName()).find();
         } else {
             throw new IllegalArgumentException("wrong key word");
         }
+        return conditions;
+    }
+
+    private void record(String path) {
+        try (PrintWriter out = new PrintWriter(new FileWriter(path, StandardCharsets.UTF_8), true)) {
+            list.forEach(out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        /** валидация шаблона параметров и получение параметров */
+        ArgsName argsName = ArgsName.of(args);
+
+        /** получаем предикат */
+        Predicate<Path> conditions = predicate(argsName.get("t"), argsName.get("n"));
 
         /** Класс для обхода дерева каталогов, ппринимает предикат и путь для записи данных в файл */
-        Search search = new Search(conditions, argsName.get("o"));
+        Find search = new Find(conditions);
 
         /** валидация директории */
         if (!Files.isDirectory(Path.of(argsName.get("d")))) {
@@ -41,7 +107,8 @@ public class Find {
         /** запуск обхода дерева */
         Files.walkFileTree(path, search);
 
-        /** запуск записи в файл */
-        search.getResult();
+        /** запись в файл */
+        search.record(argsName.get("o"));
     }
 }
+
