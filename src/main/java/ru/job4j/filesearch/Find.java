0734d24a6619cb8d286bc99d770cd7e9636f5936
AndreyPolegaev <79646307753@yandex.ru>
2021-06-28 23:16:56 +0300

    2. Поиск файлов по критерию [#783 #171492]

--- src/main/java/ru/job4j/filesearch/Find.java
+++ src/main/java/ru/job4j/filesearch/Find.java
@@ -11,60 +11,22 @@ package ru.job4j.filesearch;
  */
 
 import ru.job4j.io.ArgsName;
+
 import java.awt.geom.IllegalPathStateException;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Predicate;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
-import static java.nio.file.FileVisitResult.CONTINUE;
-
-public class Find extends SimpleFileVisitor<Path> {
-
-    /**
-     * передаем предикат в зависимости от поиска по плному имени или маске или regex
-     */
-    private final Predicate<Path> conditions;
-
-    /**
-     * сохраняем ссылки на подходящие файлы для последющей записи в файл
-     */
-    private final List<Path> list = new ArrayList<>();
-
-    public Find(Predicate<Path> conditions) {
-        this.conditions = conditions;
-    }
-
-    @Override
-    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        if (conditions.test(file)) {
-            list.add(file.toAbsolutePath());
-        }
-        return super.visitFile(file, attrs);
-    }
-
-    @Override
-    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-        return CONTINUE;
-    }
+public class Find {
 
     private static Predicate<Path> predicate(String type, String typeName) {
+
         Predicate<Path> conditions;
         /** типа файла: маска, полное имя или регулярное выражение */
         if (type.equals("mask")) {
             String rsl = typeName.replace("*", ".*").replace("?", ".");
-            rsl = rsl
-                    .chars()
-                    .mapToObj(s -> (char) s)
-                    .map(String::valueOf)
-                    .collect(Collectors.joining("", "^", "$"));
+            rsl = String.join("", "^", rsl, "$");
             Pattern regexp = Pattern.compile(rsl); // получаем регулярное выражение из маски
             conditions = p -> regexp.matcher(p.toFile().getName()).find();
         } else if (type.equals("name")) {
@@ -78,29 +40,22 @@ public class Find extends SimpleFileVisitor<Path> {
         return conditions;
     }
 
-    private void record(String path) {
-        try (PrintWriter out = new PrintWriter(new FileWriter(path, StandardCharsets.UTF_8), true)) {
-            list.forEach(out::println);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) throws IOException {
 
         /** валидация шаблона параметров и получение параметров */
         ArgsName argsName = ArgsName.of(args);
 
-        /** получаем предикат */
-        Predicate<Path> conditions = predicate(argsName.get("t"), argsName.get("n"));
-
-        /** Класс для обхода дерева каталогов, ппринимает предикат и путь для записи данных в файл */
-        Find search = new Find(conditions);
-
         /** валидация директории */
         if (!Files.isDirectory(Path.of(argsName.get("d")))) {
             throw new IllegalPathStateException("NOT A DIRECTORY");
         }
+
+        /** получаем предикат */
+        Predicate<Path> conditions = predicate(argsName.get("t"), argsName.get("n"));
+
+        /** Класс для обхода дерева каталогов, ппринимает предикат и путь для записи данных в файл */
+        Search search = new Search(conditions, argsName.get("o"));
+
         /** директтория в которой начинать поиск */
         Path path = Paths.get(argsName.get("d"));
 
@@ -108,7 +63,7 @@ public class Find extends SimpleFileVisitor<Path> {
         Files.walkFileTree(path, search);
 
         /** запись в файл */
-        search.record(argsName.get("o"));
+        search.getResult();
     }
 }
 
